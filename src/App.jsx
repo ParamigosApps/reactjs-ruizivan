69e2b54b37a1b004f4588a0f259f10522696b5e5
Ivan <ivangabrielruiz1@gmail.com>
2025-11-03 04:16:43 -0300

    1ra entrega

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>React Js - 1ra Entrega</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          Conteo de clicks: {count}
+        </button>
+        <p>Primera entrega, para el profe con mucho cariño</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
