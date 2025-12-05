89307d224c105a500aec54a0a5817e707cf8d4a6
Ivan <ivangabrielruiz1@gmail.com>
2025-12-05 19:38:09 -0300

    tp1

--- src/components/ItemContainer.jsx
+++ src/components/ItemContainer.jsx
@@ -1,10 +1,8 @@
-// src/components/ItemContainer.jsx
-
 export default function ItemContainer({ greeting }) {
   return (
     <section className="item-container">
       <h1>{greeting}</h1>
-      <p>Muy pronto vas a poder ver acá nuestro catálogo de productos.</p>
+      <p>Muy pronto vas a poder acceder a nuestro catálogo de productos.</p>
     </section>
   )
 }
